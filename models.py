a2401f510140d629a3cf53f3014596571ca296fa
kondyurin <a.kondyurin@yandex.ru>
2020-04-17 01:39:50 +0300

    Add models

---
+++ models.py
@@ -0,0 +1,44 @@
+teachers_goals = db.Table('teachers_goals',
+                          db.Column(db.Integer, db.ForeignKey('teachers.id')),
+                          db.Column(db.Integer, db.ForeignKey('goals.id'))
+                          )
+
+
+class Teacher(db.Model):
+    __tablename__ = 'teachers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False, unique=True)
+    about = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    picture_src = db.Column(db.String, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    free = db.Column(db.String, nullable=False)
+    booking = db.relationship('Booking', back_populates='teacher')
+    goals = db.relationship('Goal', secondary='teachers_goals', back_populates='teachers')
+
+
+class Booking(db.Model):
+    __tablename__ = 'bookings'
+
+    id = db.Column(db.Integer, primary_key=True)
+    teacher = db.relationship('Teacher', back_populates='booking')
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'))
+
+
+class Goal(db.Model):
+    __tablename__ = 'goals'
+
+    id = db.Column(db.Integer, primary_key=True)
+    goal = db.Column(db.String, nullable=False)
+    teachers = db.relationship('Teacher', secondary='teachers_goals', back_populates='goals')
+
+
+class Request(db.Model):
+    __tablename__ = 'requests'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    goal = db.Column(db.String, nullable=False)
+    phone = db.Column(db.String, nullable=False, unique=True)
+    time = db.Column(db.String, nullable=False)
