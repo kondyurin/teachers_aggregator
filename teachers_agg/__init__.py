4d32952d56b45e53ccb4030146f086c7cc556426
kondyurin <a.kondyurin@yandex.ru>
2020-04-17 03:47:50 +0300

    Change db name

--- teachers_agg/__init__.py
+++ teachers_agg/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.secret_key = 'abrikos'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
