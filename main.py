4ab6c21256c16a383ae58912c00cc9d45a10f70a
kondyurin <a.kondyurin@yandex.ru>
2020-04-17 14:01:41 +0300

    Add db, migrate command

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from teachers_agg import app
+from teachers_agg import app, db
+from teachers_agg.models import teachers_goals, Teacher, Booking, Goal, Request
+from flask_script import Manager
+from flask_migrate import MigrateCommand
 
 
-app.run()
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+
+if __name__ == "__main__":
+    manager.run()
