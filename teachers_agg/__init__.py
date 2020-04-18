c6f6e6735ffa51b55b095261b89c1153da73e3b3
kondyurin <a.kondyurin@yandex.ru>
2020-04-18 13:50:20 +0300

    Add teachers from json to db

--- teachers_agg/__init__.py
+++ teachers_agg/__init__.py
@@ -1,18 +1,57 @@
+import json
+
 from flask import Flask
 from flask_migrate import Migrate, MigrateCommand
 from flask_sqlalchemy import SQLAlchemy
 
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy import create_engine
+from sqlalchemy.engine import reflection
+
 
 app = Flask(__name__)
 
+db_path = 'sqlite:///db.db'
 app.secret_key = 'abrikos'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
-from . import views
+
+from teachers_agg import views, models
+from teachers_agg.models import Teacher
+
+engine = create_engine(db_path)
+
+def add_teachers_to_db():
+    """
+    Add teachers from JSON to table Teacher.
+    """
+    with open('teachers_agg/teachers.json') as json_file:
+        profiles = json.load(json_file)
+    for item in profiles['teachers']:
+        teacher = Teacher(id=item['id'], 
+                          name=item['name'],
+                          about=item['about'],
+                          rating=item['rating'],
+                          picture_src=item['picture'],
+                          price=item['price'],
+                          free=str(item['free']))
+        db.session.add(teacher)
+    try:
+        db.session.commit()
+    except IntegrityError:
+        db.session.rollback()
 
 
+# def is_db_empty():
+#     table_names = reflection.Inspector.from_engine(engine).get_table_names()
+#     print(table_names)
+#     is_empty = table_names == []
+#     is_empty = engine.has_table('Teacher')
+
+
+# is_db_empty()
