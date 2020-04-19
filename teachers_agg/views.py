bfff1944cbd4d3df29fdbc28410c6bfaa001b1d6
kondyurin <a.kondyurin@yandex.ru>
2020-04-19 04:50:08 +0300

    Get goals from db; edit request template

--- teachers_agg/views.py
+++ teachers_agg/views.py
@@ -6,7 +6,7 @@ from sqlalchemy.sql.expression import func
 
 from .data import goals
 from teachers_agg import app, db
-from teachers_agg.models import Teacher, Booking
+from teachers_agg.models import Teacher, Booking, Goal
 
 
 @app.route("/")
@@ -14,11 +14,13 @@ def main():
     random_profiles = Teacher.query.order_by(func.random()).limit(6).all()
     return render_template("index.html", random_profiles=random_profiles)
 
+
 @app.route("/goals/<goal>/")
 def get_goal(goal):
     current_goal = goals[goal]
     profiles = get_teachers_profiles()
     return render_template("goal.html", goals=goals, goal=goal, current_goal=current_goal, profiles=profiles)
+
 
 @app.route("/profiles/<int:id>/")
 def profile(id):
@@ -26,9 +28,12 @@ def profile(id):
     teacher_free = json.loads(teacher.free)
     return render_template("profile.html", id=id, teacher=teacher, teacher_free=teacher_free)
 
+
 @app.route("/request/")
 def request_select():
+    goals = Goal.query.all()
     return render_template("request.html", goals=goals)
+
 
 @app.route("/request_done/", methods=['GET', 'POST'])
 def request_done():
@@ -46,10 +51,12 @@ def request_done():
             request_data_json = json.dump(request_data, f)
         return render_template("request_done.html", request_data=request_data)
 
+
 @app.route("/booking/<int:id>/<day>/<hour>/")
 def booking(id, day, hour):
     profile = Teacher.query.get_or_404(id)
     return render_template("booking.html", id=id, day=day, hour=hour, profile=profile)
+
 
 @app.route("/booking_done/", methods=['GET','POST'])
 def booking_done():
